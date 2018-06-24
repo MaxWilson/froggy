6d87e6adb40b696c4fdab57e6fb0aa397ad04452
Max Wilson <wilson.max@gmail.com>
2018-06-23 18:02:20 -0700

    wip

--- Froggy.Test/Dnd5E.Tests.fs
+++ Froggy.Test/Dnd5E.Tests.fs
@@ -11,13 +11,19 @@ let UsageTest() =
   let output = ref ""
   ctx.UpdateStatus <- fun summary -> output := summary
   let proc cmd = ParseContext.Init cmd |> Froggy.Dnd5e.CharGen.parse |> ctx.Execute
+  proc "name Mengar the Magnificent"
+  Assert.Contains("Name: Mengar the Magnificent", !output)
+
+[<Fact(DisplayName="Usage tests: verify corner cases for parse commands")>]
+let CornerCasees() =
+  let ctx = StatBank()
+  let output = ref ""
+  ctx.UpdateStatus <- fun summary -> output := summary
+  let proc cmd = ParseContext.Init cmd |> Froggy.Dnd5e.CharGen.parse |> ctx.Execute
   Assert.Equal("", !output)
   proc "new"
   Assert.Equal("Name: Unnamed", !output)
-  proc "name Mengar the Magnificent"
-  Assert.Contains("Name: Mengar the Magnificent", !output)
   proc "name     Uncanny John, eater of fish  " // deliberate extraneous whitespace and commas
   Assert.Contains("Name: Uncanny John, eater of fish", !output)
   proc "namebob" // not a valid name, should change nothing
   Assert.Contains("Name: Uncanny John, eater of fish", !output)
-
