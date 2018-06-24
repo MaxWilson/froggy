7d08f4399fca6a4d2cc058cd60d29ee7844f825b
Max Wilson <wilson.max@gmail.com>
2018-06-23 17:11:10 -0700

    Refactor tests into separate files for DnD vs. Froggy (Packrat)

--- Froggy.Test/Packrat.Tests.fs
+++ Froggy.Test/Packrat.Tests.fs
@@ -47,14 +47,4 @@ let ``Packrat tests: grammar for converting yesno to binary-ish``() =
     | YesNos(_) -> failwith "expected parse failure"
     | _ -> ()
 
-[<Fact>]
-let ``Usage tests: verify that chargen commands can be parsed correctly``() =
-  let ctx = StatBank()
-  let output = ref ""
-  ctx.UpdateStatus <- fun summary -> output := summary
-  let proc cmd = Froggy.Dnd5e.CharGen.parse cmd |> ctx.Execute
-  proc "new"
-  Assert.Equal("", !output)
-  proc "name Mengar the Magnificent"
-  Assert.Contains("Name: Mengar the Magnificent", !output)
 
