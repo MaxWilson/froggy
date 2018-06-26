bde248c450ba94d428852b69c1c71446d2d4ea48
Max Wilson <wilson.max@gmail.com>
2018-06-26 05:51:12 -0700

    Restore EncounterGen

--- FroggyWeb/src/App.fs
+++ FroggyWeb/src/App.fs
@@ -1,4 +1,4 @@
-module App.View
+module Froggy.Web
 
 open Elmish
 open Elmish.Browser.Navigation
@@ -7,7 +7,6 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
 open Fable.Import.Browser
-open Types
 open App.State
 open Global
 
@@ -15,65 +14,58 @@ importAll "../sass/main.sass"
 
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
+open Froggy.Common
+open EncounterGen
+module R = Fable.Helpers.React
 
-let menuItem label page currentPage =
-    li
-      [ ]
-      [ a
-          [ classList [ "is-active", page = currentPage ]
-            Href (toHash page) ]
-          [ str label ] ]
+type Model = { number: int; level: int; difficulty: int; msg: string }
+type Msg = Generate | AlterLevel of int | AlterPCNumber of int | AlterDifficulty of int
 
-let menu currentPage =
-  aside
-    [ ClassName "menu" ]
-    [ p
-        [ ClassName "menu-label" ]
-        [ str "General" ]
-      ul
-        [ ClassName "menu-list" ]
-        [ menuItem "Home" Home currentPage
-          menuItem "Counter sample" Counter currentPage
-          menuItem "About" Page.About currentPage ] ]
+let update msg model =
+  match msg with
+  | AlterLevel(n) -> { model with level = model.level + n |> max 1 |> min 20 }
+  | AlterPCNumber(n) -> { model with number = model.number + n |> max 1 }
+  | AlterDifficulty(n) -> { model with difficulty = model.difficulty + n |> max 1 }
+  | Generate ->
+    { model with msg = generateVariant monsterParties (List.init model.number (thunk model.level)) model.difficulty }
 
-let root model dispatch =
+let init() = { msg = "Push the button to generate an encounter"; level = 1; difficulty = 4; number = 4 }
 
-  let pageHtml =
-    function
-    | Page.About -> Info.View.root
-    | Counter -> Counter.View.root model.counter (CounterMsg >> dispatch)
-    | Home -> Home.View.root model.home (HomeMsg >> dispatch)
+let descr model =
+  let diff =
+    match model.difficulty with
+    | n when n <= 4 -> ["Easy";"Medium";"Hard";"Deadly"].[n-1]
+    | n -> "Deadly x" + (n - 3).ToString()
+  let lev =
+    match model.level with
+    | 1 -> "1st"
+    | 2 -> "2nd"
+    | n -> (n.ToString()) + "th"
+  sprintf "%s encounter for %i %s-level PCs" diff model.number lev
 
-  div
-    []
-    [ div
-        [ ClassName "navbar-bg" ]
-        [ div
-            [ ClassName "container" ]
-            [ Navbar.View.root ] ]
-      div
-        [ ClassName "section" ]
-        [ div
-            [ ClassName "container" ]
-            [ div
-                [ ClassName "columns" ]
-                [ div
-                    [ ClassName "column is-3" ]
-                    [ menu model.currentPage ]
-                  div
-                    [ ClassName "column" ]
-                    [ pageHtml model.currentPage ] ] ] ] ]
+let view model d =
+  R.div []
+      [ R.div [] [ R.str <| descr model ]
+        R.button [ OnClick (fun _ -> d <| AlterLevel +1) ] [ R.str "Add PC" ]
+        R.button [ OnClick (fun _ -> d <| AlterLevel -1) ] [ R.str "Remove PC" ]
+        R.button [ OnClick (fun _ -> d <| AlterPCNumber +1) ] [ R.str "Stronger PCs" ]
+        R.button [ OnClick (fun _ -> d <| AlterPCNumber -1) ] [ R.str "Weaker PCs" ]
+        R.button [ OnClick (fun _ -> d <| AlterDifficulty +1) ] [ R.str "Harder" ]
+        R.button [ OnClick (fun _ -> d <| AlterDifficulty -1) ] [ R.str "Easier" ]
+        R.button [ OnClick (fun _ -> Generate |> d) ] [ R.str "Go" ]
+        R.div [] [ R.str <| model.msg.Replace("\n", "<p>") ]
+]
 
 open Elmish.React
 open Elmish.Debug
 open Elmish.HMR
 
 // App
-Program.mkProgram init update root
-|> Program.toNavigable (parseHash pageParser) urlUpdate
+Program.mkSimple init update view
 #if DEBUG
 |> Program.withDebugger
 |> Program.withHMR
 #endif
 |> Program.withReact "elmish-app"
 |> Program.run
+
