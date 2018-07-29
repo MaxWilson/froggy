79489794303d9b5407dbf1f847cb252080c5b343
Max Wilson <wilson.max@gmail.com>
2018-07-29 16:56:25 -0700

    wip broken

--- Froggy/Data.fs
+++ Froggy/Data.fs
@@ -24,13 +24,51 @@ type Data<'propertyMetadataType, 'propertyValueType> = {
   mapping: StatBank<'propertyValueType>
   properties: Map<string, 'propertyMetadataType>
   parentScope: Data<'propertyMetadataType, 'propertyValueType> option
+  round: int option
 }
-
-let empty = { roster = Map.empty; reverseRoster = Map.empty; mapping = Map.empty; properties = Map.empty; parentScope = None }
-let getParent<'mt, 't> : RecursiveOptionLens<Data<'mt, 't>> = Lens.lens (fun x -> x.parentScope) (fun v x -> { x with parentScope = v })
+type PropertyMetadata<'store, 'union, 't> = { Name: PropertyName; Create: 't -> PropertyValueComponent<'store, 'union>; Get: 'union -> 't option }
+module PropertyMetadata =
+  let create name createFunction getFunction =
+    { Name = name; Create = createFunction; Get = getFunction }
+module Data =
+  let empty = { roster = Map.empty; reverseRoster = Map.empty; mapping = Map.empty; properties = Map.empty; parentScope = None; round = None }
+  let getParent<'mt, 't> : RecursiveOptionLens<Data<'mt, 't>> = Lens.lens (fun x -> x.parentScope) (fun v x -> { x with parentScope = v })
+  let newRound d = { d with round = Some ((defaultArg d.round 0) + 1) }
+  let set prop (id: Id) scope v d =
+    let currentValue =
+      match d.mapping |> Map.tryFind (id, prop.Name) with
+      | Some pv -> pv
+      | None -> PropertyValue.empty
+    let pv' = PropertyValue.add scope v currentValue
+    { d with mapping = d.mapping |> Map.add (id, prop.Name) pv' }
 
 module SimpleProperties =
-  type PropertyValueUnion = Text of string | Number of int
+  type Conditions = {
+                      CharmedBy: Id list; Blinded: bool; Deafened: bool; Exhausted: int; FrightenedOf: Id list;
+                      Grappled: bool; Incapacitated: bool; Invisible: bool; Paralyzed: bool; Petrified: bool;
+                      Poisoned: bool; Prone: bool; Restrained: bool; Stunned: bool; Unconscious: bool; }
+  module Conditions =
+    let empty = { CharmedBy = []; Blinded = false; Deafened = false; Exhausted = 0; FrightenedOf = [];
+                  Grappled = false; Incapacitated = false; Invisible = false; Paralyzed = false; Petrified = false;
+                  Poisoned = false; Prone = false; Restrained = false; Stunned = false; Unconscious = false }
+    let computeClosure (c:Conditions) =
+      { c with Incapacitated = c.Incapacitated || c.Petrified || c.Paralyzed || c.Stunned || c.Unconscious;
+                Prone = c.Prone || c.Unconscious } // todo: how to prevent prone from ending just because Unconscious ends
+    let isIncapacitated c =
+      (computeClosure c).Incapacitated
+
+  type PropertyValueUnion = Condition of Conditions | Number of int
+  let addTemporaryCondition duration addCondition (PropertyValue cs) =
+    PropertyValue(((Temporary duration), (Transform addCondition))::cs)
+  let addProne c = { c with Prone = true }
+  let isRound x s = s.round = Some x
+  let pv = PropertyValue.empty |> addTemporaryCondition (isRound 1) addProne
+  let hp = PropertyMetadata.create "HP" id (function Number n -> Some n | v -> None)
+  let data = Data.empty |> Data.set hp 1 Lasting (Value (Number 4))
+  let v = (PropertyValue.computeCurrentValue Data.getParent data pv)
+  printf "Are we prone? %A" v
+
+  type PropertyValueUnion = Text of string | Number of int | Conditions of Conditions
   let asNumber = function Number n -> n | v -> failwithf "Invalid cast: %A is not a number" v
   let asText = function Text n -> n | v -> failwithf "Invalid cast: %A is not text" v
 
--- Froggy/Properties.fs
+++ Froggy/Properties.fs
@@ -8,30 +8,32 @@ module Froggy.Properties
 
   type RoundNumber = int
   type PropertyScope<'store> = Permanent | Lasting | Temporary of ('store -> bool)
-  type PropertyValueComponent<'store, 'args, 't> = Value of 't | Transform of ('t -> 't) | Computed of ('args -> 'store -> 't)
-  type PropertyValue<'store, 'args, 't> = PropertyValue of (PropertyScope<'store> * PropertyValueComponent<'store, 'args, 't>) list
-  let computeValue (scopeFilter: PropertyScope<_> -> bool) (parentLens: Lens<'store, 'store option, 'store option, 'store>) (args: 'args) (store: 'store) (PropertyValue(vs): PropertyValue<'store, 'args, 't>) =
-    let rec applyValue v rest =
-      match v with
-      | Value v -> v
-      | Transform t ->
-        let v = eval rest
-        t v
-      | Computed f -> f args store
-    and eval =
-      function
-        | (scope, _)::rest when scopeFilter scope = false ->
-          // do NOT apply since it's for a different scope
-          eval rest
-        | ((Permanent | Lasting), v)::rest ->
-          applyValue v rest
-        | (Temporary(pred), v)::rest when pred store ->
-          applyValue v rest
-        | (Temporary(_), _)::rest ->
-          // do NOT apply since pred was false
-          eval rest
-        | v -> matchfail v
-    eval vs
-  let computePermanentValue parentLens args store pv = computeValue (function Permanent -> true | _ -> false) parentLens args store pv
-  let computeCurrentValue parentLens args store pv = computeValue (thunk true) parentLens args store pv
-
+  type PropertyValueComponent<'store, 't> = Value of 't | Transform of ('t -> 't)
+  type PropertyValue<'store, 't> = PropertyValue of (PropertyScope<'store> * PropertyValueComponent<'store, 't>) list
+  module PropertyValue =
+    let empty = PropertyValue([])
+    let computeValue (scopeFilter: PropertyScope<_> -> bool) (parentLens: Lens<'store, 'store option, 'store option, 'store>) (store: 'store) (PropertyValue(vs): PropertyValue<'store, 't>) =
+      let rec applyValue v rest =
+        match v with
+        | Value v -> v
+        | Transform t ->
+          let v = eval rest
+          t v
+      and eval =
+        function
+          | (scope, _)::rest when scopeFilter scope = false ->
+            // do NOT apply since it's for a different scope
+            eval rest
+          | ((Permanent | Lasting), v)::rest ->
+            applyValue v rest
+          | (Temporary(pred), v)::rest when pred store ->
+            applyValue v rest
+          | (Temporary(_), _)::rest ->
+            // do NOT apply since pred was false
+            eval rest
+          | v -> matchfail v
+      eval vs
+    let computePermanentValue parentLens store pv = computeValue (function Permanent -> true | _ -> false) parentLens store pv
+    let computeCurrentValue parentLens store pv = computeValue (thunk true) parentLens store pv
+    let add scope pv (PropertyValue pvs) =
+      PropertyValue((scope, pv)::pvs)
